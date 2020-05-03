e6c50bead8eadbb3bc9115d07e274f2fab757820
Patrick Metz <patrickmetz@web.de>
2020-05-03 20:20:17 +0200

    -Testordner in oberste Projektebene verschoben - erster Test scheitert nun korrekt :D

--- test/ParkhausTest.java
+++ test/ParkhausTest.java
@@ -1,12 +1,13 @@
-package Tests;
-
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.fail;
+
 public class ParkhausTest {
+
     @Test
     @DisplayName("Einfahrt mit leerem Parkhaus ist erfolgreich")
-    void einfahrt_mitLeeremParkhaus_istErfolgreich () {
-
+    void einfahrt_mitLeeremParkhaus_istErfolgreich() {
+        fail();
     }
 }
