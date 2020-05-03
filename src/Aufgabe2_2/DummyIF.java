3036e1c937b448a0d2bb36c5095fba53a7273348
Patrick Metz <patrickmetz@web.de>
2020-05-03 12:17:12 +0200

    vorigen Zustand wiederhergestellt

--- src/Aufgabe2_2/DummyIF.java
+++
@@ -1,5 +0,0 @@
-package Aufgabe2_2;
-
-public interface DummyIF {
-    //angelegt, damit git das verzeichnis commited
-}
--- test/Aufgabe2_2/DummyIFTest.java
+++
@@ -1,5 +0,0 @@
-package Aufgabe2_2;
-
-public class DummyIFTest {
-    //angelegt, damit git das verzeichnis commited
-}
