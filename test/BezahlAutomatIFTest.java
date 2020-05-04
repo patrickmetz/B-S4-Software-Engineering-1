abd46aef0a554ef8e896109430638713ebf25fc0
johannes <johannes.kratzsch@gmail.com>
2020-05-04 12:39:58 +0200

    Add BezahlAutomatIFTests

--- test/BezahlAutomatIFTest.java
+++ test/BezahlAutomatIFTest.java
@@ -1,15 +1,22 @@
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class BezahlAutomatIFTest {
+    BezahlAutomatIF automat;
+    ParkticketIF ticket;
 
     @BeforeEach
     void setUp() {
+        automat = new BezahlAutomat();
+        ticket = new Parkticket();
     }
 
     @Test
-    void bezahlen() {
+    @DisplayName("Bezahlen eines Parktickets läuft erfolgreich")
+    void bezahlen_einesParktickets_laeuftErfolgreich() {
+        assertTrue(automat.bezahlen(ticket));
     }
 }
