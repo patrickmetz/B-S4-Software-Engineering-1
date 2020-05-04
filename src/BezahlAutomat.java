269d29186b9c51c2b36f3d0f562d94bfc31815df
johannes <johannes.kratzsch@gmail.com>
2020-05-04 12:55:57 +0200

    Improve naming

--- src/BezahlAutomat.java
+++ src/BezahlAutomat.java
@@ -6,7 +6,7 @@ public class BezahlAutomat implements BezahlAutomatIF {
     }
 
     @Override
-    public Double getPreis(ParkticketIF parkTicket) {
+    public Double getStundenPreis(ParkticketIF parkTicket) {
         return null;
     }
 }
