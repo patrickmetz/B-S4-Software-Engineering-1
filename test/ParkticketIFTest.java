bd6208e7cfc09d3450f493109eeabee09fcb42da
johannes <johannes.kratzsch@gmail.com>
2020-05-04 11:11:03 +0200

    Add empty tests classes

---
+++ test/ParkticketIFTest.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ParkticketIFTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void setBezahlt() {
+    }
+
+    @Test
+    void getPreis() {
+    }
+}
