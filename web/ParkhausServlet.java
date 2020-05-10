3b40acc52d2837c2fc711c9c2be44da90e5714fd
Patrick Metz <patrickmetz@web.de>
2020-05-10 22:11:34 +0200

    POST- und GET-Requests können nun ausgelesen werden

--- web/ParkhausServlet.java
+++ web/ParkhausServlet.java
@@ -3,42 +3,78 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
+import java.util.Arrays;
 import java.util.HashMap;
 
 @WebServlet("/ParkhausServlet")
 public class ParkhausServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HashMap<String, String> bodyMap = getPostHashMap(request);
 
+        System.out.println("POST-command: " + bodyMap.get("cmd"));
+        System.out.println("POST-csv: " + bodyMap.get("csv"));
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        HashMap<String, String> query = getQueryHashMap(request);
+        HashMap<String, String> queryMap = getQueryHashMap(request);
 
-        if ("config".equals(query.get("cmd"))) {
+        if (queryMap.get("cmd").equals("config")) {
             // see https://kaul.inf.h-brs.de/se/#app-content-4-0&03_Technologien=page-53
             sendResponse(response, "100,6,24,100,10");
         } else {
-            System.out.println("Invalid Command: " + request.getQueryString());
+            System.out.println("Invalid GET-command: " + request.getQueryString());
         }
     }
 
-    private void sendResponse(HttpServletResponse response, String config) throws IOException {
-        response.setContentType("text/html");
-        response.setCharacterEncoding("UTF-8");
-        PrintWriter out = response.getWriter();
-        out.println(config);
-    }
-
-    private HashMap<String, String> getQueryHashMap(HttpServletRequest request) {
-        HashMap<String, String> query = new HashMap<>();
+    private static HashMap<String, String> getQueryHashMap(HttpServletRequest request) {
+        HashMap<String, String> map = new HashMap<>();
 
         for (String splitByAnd : request.getQueryString().split("&")) {
             String[] splitByEquals = splitByAnd.split("=");
-            query.put(splitByEquals[0], splitByEquals[1]);
+            map.put(splitByEquals[0], splitByEquals[1]);
         }
 
-        return query;
+        return map;
+    }
+
+    private static HashMap<String, String> getPostHashMap(HttpServletRequest request) throws IOException {
+        HashMap<String, String> map = new HashMap<>();
+        String[] splitByFirstComma = getPostBody(request).split(",", 2);
+
+        map.put("cmd", splitByFirstComma[0]);
+        map.put("csv", splitByFirstComma[1]);
+
+        return map;
+    }
+
+    private static String getPostBody(HttpServletRequest request) throws IOException {
+        StringBuilder stringBuilder = new StringBuilder();
+        BufferedReader bufferedReader = null;
+        try {
+            InputStream inputStream = request.getInputStream();
+            if (inputStream != null) {
+                bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                char[] charBuffer = new char[128];
+                int bytesRead = -1;
+                while ((bytesRead = bufferedReader.read(charBuffer)) > 0) {
+                    stringBuilder.append(charBuffer, 0, bytesRead);
+                }
+            } else {
+                stringBuilder.append("");
+            }
+        } finally {
+            if (bufferedReader != null) {
+                bufferedReader.close();
+            }
+        }
+        return stringBuilder.toString();
+    }
+
+    private static void sendResponse(HttpServletResponse response, String htmlContent) throws IOException {
+        response.setContentType("text/html");
+        response.setCharacterEncoding("UTF-8");
+        PrintWriter out = response.getWriter();
+        out.println(htmlContent);
     }
 }
