add2a064c49795f05218998bfff3133ce83fde55
Patrick Metz <patrickmetz@web.de>
2020-05-10 20:53:52 +0200

    Parkhaus-Servlet repariert

--- web/ParkhausServlet.java
+++ web/ParkhausServlet.java
@@ -5,13 +5,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "parkhaus")
+@WebServlet("/ParkhausServlet")
 public class ParkhausServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+            System.out.println("Invalid Command: " + request.getQueryString());
     }
 }
