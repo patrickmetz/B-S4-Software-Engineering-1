3e7b30e19f61cce2cb11107f4975b5b59c325cf6
Patrick Metz <patrickmetz@web.de>
2020-05-10 21:11:22 +0200

    Get-Request nach "&" und "=" gesplittet

--- web/ParkhausServlet.java
+++ web/ParkhausServlet.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
 
 @WebServlet("/ParkhausServlet")
 public class ParkhausServlet extends HttpServlet {
@@ -12,6 +13,19 @@ public class ParkhausServlet extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HashMap<String, String> query = new HashMap<>();
+
+        //todo: put into static tool class and write a test for it
+        for (String splitByAnd : request.getQueryString().split("&")) {
+            String[] splitByEquals = splitByAnd.split("=");
+            query.put(splitByEquals[0], splitByEquals[1]);
+        }
+
+        if ("config".equals(query.get("cmd"))) {
+            System.out.println("Known Command: " + request.getQueryString());
+
+        } else {
             System.out.println("Invalid Command: " + request.getQueryString());
+        }
     }
 }
