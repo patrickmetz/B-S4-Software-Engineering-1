d53a38950137bc88ffa2fe90d8134514bb66a85c
Patrick Metz <patrickmetz@web.de>
2020-06-21 21:05:12 +0200

    - Abstrakes Model implementiert

---
+++ src/AbstractModel.java
@@ -0,0 +1,22 @@
+import java.util.List;
+
+public abstract class AbstractModel implements ModelIF {
+    private List<ViewIF> views;
+
+    @Override
+    public void anmelden(ViewIF view) {
+        views.add(view);
+    }
+
+    public void aktualisieren() {
+        for (ViewIF view : views) {
+            view.aktualisieren();
+        }
+    }
+
+    @Override
+    public abstract List<Object> gibDaten();
+
+    @Override
+    public abstract void speichereDaten(List<Object> daten);
+}
