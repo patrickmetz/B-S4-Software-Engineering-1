ecc4465ee0b4ae7fa5002204b2d04e585a5185a9
Patrick Metz <patrickmetz@web.de>
2020-06-22 20:50:39 +0200

    - Tutorial 4/6 abgeschlossen

--- angular/parkhaus/src/app/heroes/heroes.component.ts
+++ angular/parkhaus/src/app/heroes/heroes.component.ts
@@ -1,7 +1,8 @@
-import {Component, OnInit} from '@angular/core';
-import {Hero} from '../hero';
-import { HEROES } from '../mock-heroes';
+import { Component, OnInit } from '@angular/core';
+
+import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,29 +10,24 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  heroes: Hero[];
 
   selectedHero: Hero;
 
+  heroes: Hero[];
+
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
+
+  ngOnInit() {
+    this.getHeroes();
+  }
+
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
-  }
-
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  }
-
-  constructor(private heroService: HeroService) {
+    this.messageService.add(`HeroService: Selected hero id=${hero.id}`);
   }
 
   getHeroes(): void {
     this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes);
   }
-
-  ngOnInit(): void {
-    this.getHeroes();
-  }
-
 }
