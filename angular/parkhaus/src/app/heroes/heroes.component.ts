769d841829b09df2e90e928d520bad56a5c4c386
Patrick Metz <patrickmetz@web.de>
2020-06-22 21:52:56 +0200

    - Tutorial 5/6 abgeschlossen

--- angular/parkhaus/src/app/heroes/heroes.component.ts
+++ angular/parkhaus/src/app/heroes/heroes.component.ts
@@ -21,10 +21,10 @@ export class HeroesComponent implements OnInit {
     this.getHeroes();
   }
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroService: Selected hero id=${hero.id}`);
-  }
+  // onSelect(hero: Hero): void {
+  //   this.selectedHero = hero;
+  //   this.messageService.add(`HeroService: Selected hero id=${hero.id}`);
+  // }
 
   getHeroes(): void {
     this.heroService.getHeroes()
