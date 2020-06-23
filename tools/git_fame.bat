9e31b88e46d48605fdea7a1a8fcb77e4869473ad
Patrick Metz <patrickmetz@web.de>
2020-06-23 18:42:49 +0200

    - git-fame korrigiert

--- tools/git_fame.bat
+++ tools/git_fame.bat
@@ -1,2 +1,10 @@
 cd ..
-git-fame --excl "angular|docs|tutorial"
+git-fame --excl "angular|docs|tutorial|lib|web"
+
+cd ./angular/parkhaus/src/app/preisformular
+git-fame
+cd ../../../../../
+
+cd ./web
+git-fame --excl "tutorial"
+
