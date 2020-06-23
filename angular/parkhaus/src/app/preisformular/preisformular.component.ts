faed6727cdcef1f613aafa3add849e11b8bbadbf
Patrick Metz <patrickmetz@web.de>
2020-06-23 15:04:49 +0200

    - Preisformular-Komponente ergänzt

---
+++ angular/parkhaus/src/app/preisformular/preisformular.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-preisformular',
+  templateUrl: './preisformular.component.html',
+  styleUrls: ['./preisformular.component.css']
+})
+export class PreisformularComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
