9c3d4a9383a6c08426f4d7d26686d6776caaedb0
Patrick Metz <patrickmetz@web.de>
2020-06-29 23:50:28 +0200

    - defekte Readme-Links, wenn zwei Mal "src" oder "source" enthalten, repariert: Angulars "source" verzeichnis in "files" umbenannt...

--- tools/build_angular_preisformular.bat
+++ tools/build_angular_preisformular.bat
@@ -1,5 +1,5 @@
 rem kompiliert die angular-app "parkhaus" in tomcat's web-ordner
 
-cd ../angular/parkhaus/source
+cd ../angular/parkhaus/files
 ng build --prod --delete-output-path false --output-hashing none --base-href /
 
