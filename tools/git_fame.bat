0bf1878a14de8e1898c1fdb6137861ee66d67456
Patrick Metz <patrickmetz@web.de>
2020-06-30 21:33:22 +0200

    git-fame-Batchdatei upgedatet

--- tools/git_fame.bat
+++ tools/git_fame.bat
@@ -1,6 +1,6 @@
 cd ..
 
 git-fame --excl "angular|docs|lib|web|README"
-git-fame --incl "web/index.jsp|angular/parkhaus/src/app/preisformular"
-git-fame --incl "web/index.jsp|angular/parkhaus/src/app/manager-ansicht"
+git-fame --incl "web/index.jsp|angular/parkhaus/files/app/preisformular"
+git-fame --incl "angular/parkhaus/files/app/manager-ansicht"
 
