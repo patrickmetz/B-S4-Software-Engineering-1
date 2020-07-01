12ecb23b917d907e751ffef93e38f29db802cd96
johannes <johannes.kratzsch@gmail.com>
2020-07-01 21:24:33 +0200

    Modify script for author export

--- tools/Eigenleistungen.py
+++ tools/Eigenleistungen.py
@@ -5,15 +5,20 @@ import re
 
 author = re.compile(r'@author (.*)')
 
-for root, dirs, files in os.walk("../src"):
-    for file in files:
-        if file.endswith(".java"):
-            path = os.path.join(root, file)
-            content = open(path).read()
-            matches = author.findall(content)
-            names = ""
+def printFiles(dir):
+    for root, dirs, files in os.walk(dir):
+        for file in files:
+            if file.endswith(".java"):
+                path = os.path.join(root, file)
+                content = open(path).read()
+                matches = author.findall(content)
+                names = ""
 
-            if matches is not None:
-                names = ','.join(str(match) for match in matches)
+                if matches is not None:
+                    names = ','.join(str(match) for match in matches)
 
-            print(path + ";" + names)
+                print(path + ";" + names)
+
+
+printFiles("../src")
+printFiles("../test")
