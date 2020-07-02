e805026d1cf50076b8c8d60d498d60c88a679415
Patrick Metz <patrickmetz@web.de>
2020-07-02 23:53:34 +0200

    Unit-Test ergänzt

--- src/test/java/preis/PreisVerwaltungModelTest.java
+++ src/test/java/preis/PreisVerwaltungModelTest.java
@@ -4,23 +4,39 @@ import kunde.KundenTyp;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * @author Patrick Metz
  */
 class PreisVerwaltungModelTest {
+
     @Test
     @DisplayName("Nach der Instanziierung sollen für alle Kundentypen initiale Preise gesetzt sein.")
-    public void PreisInitialisierungTest(){
-
-        // wir testen absichtlich keine getter und setter
-        // https://stackoverflow.com/questions/6197370/should-unit-tests-be-written-for-getter-and-setters
-
+    public void PreisInitialisierungTest() {
         PreisVerwaltungModel model = new PreisVerwaltungModel(KundenTyp.values());
 
         for (KundenTyp kundenTyp : KundenTyp.values()) {
             assertNotNull(model.getPreis(kundenTyp));
+        }
+    }
+
+    @Test
+    @DisplayName("Preise sollen korrekt per String-Hashmap gesetzt werden.")
+    public void PreisSetzen() {
+        PreisVerwaltungModel model = new PreisVerwaltungModel(KundenTyp.values());
+
+        for (KundenTyp kundenTyp : KundenTyp.values()) {
+            float neuerPreis = 1.23f;
+
+            HashMap<String, String> map = new HashMap<>();
+            map.put(kundenTyp.toString(), neuerPreis + "");
+
+            model.setPreiseAlsStringMap(map);
+
+            assertEquals(model.getPreis(kundenTyp).getBetrag(), neuerPreis);
         }
     }
 }
